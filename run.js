434f9c1ac6456b96028f6ae49778a8ab415a2cb3
Ryan Rowland <ryan.rowland@am.sony.com>
2013-12-03 07:36:29 +0000

    Cleaned up

--- run.js
+++ run.js
@@ -2,28 +2,33 @@ var request = require('request')
 var colors = require('colors')
 var fs = require('fs')
 
-var tickers;
+var url = 'http://finance.google.com/finance/info?client=ig&q=NASDAQ%3a';
 
+// Concatenate tickers.conf into a comma-delimited string
 fs.readFile('conf/tickers.conf', 'utf8', function(err, data) {
-  tickers = data.replace(/\r?\n/g, ',').replace(' ', '').toUpperCase();
-  tickers.match(/,$/g) && (tickers = tickers.slice(0, tickers.length-1));
+  url += data.replace(/\r?\n/g, ',').replace(/\s?\t/g, '').toUpperCase();
 
-  updateTickers(tickers);
+  updateTickers();
 });
 
 var updateTickers = function() {
-  request('http://finance.google.com/finance/info?client=ig&q=NASDAQ%3a' + tickers, function(err, res, body) {
+  request(url, function(err, res, body) {
+    // Convert response to JSON
     body = body.slice(3, body.length);
     var json = JSON.parse(body);
 
+    // Format timestamp
     var output = '[' + new Date().toISOString().replace(/\..+/, '').split('T')[1] + '] ';
     for(var i = 0; i < json.length; i++) {
+      // Format string with color-coded price change
       var color = (json[i].c < 0) ? 'red' : 'green';
       output += json[i].t + ': $' + json[i].l + ' (' + json[i].c[color] + ')   ';
     }
 
+    // Clear the shell and print new output
     console.log('\033[2J' + output);
 
+    // Do it again in 3 seconds
     setTimeout(updateTickers, 3000);
   });
 }
